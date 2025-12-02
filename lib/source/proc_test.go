48a55b575cc9ec20754d7cd0ca5edd4aef62a85c
Mariano Casco <mariano@mailbox.org>
2025-12-02 02:56:24 -0500

    List proc module test scenarios

--- lib/source/proc_test.go
+++ lib/source/proc_test.go
@@ -33,4 +33,46 @@ func TestAttachProc_NoOutput(t *testing.T) {
 	case <-time.After(time.Second):
 		t.Fatalf("timeout expired")
 	}
+
+	// TODO check output
+}
+
+func TestAttachProc_FailsCommandNotFound(t *testing.T) {
+	ctx := t.Context()
+	spec := NewSpec("nonexistent", "berengario", []string{})
+	src, err := attachProc(ctx, spec)
+
+	if err == nil {
+		t.Fatalf("err is nil")
+	}
+	if src != nil {
+		t.Fatalf("src not nil: %v", src)
+	}
+}
+
+func TestAttachProc_RedirectsStdout(t *testing.T) {
+}
+
+func TestAttachProc_StreamsMultipleLines(t *testing.T) {
+}
+
+func TestAttachProc_TruncatesLongLine(t *testing.T) {
+}
+
+func TestAttachProc_LastLineNoNewline(t *testing.T) {
+}
+
+func TestAttachProc_CancelContext(t *testing.T) {
+}
+
+func TestAttachProc_ExternalSigterm(t *testing.T) {
+}
+
+func TestAttachProc_ChanClosedOnExit(t *testing.T) {
+}
+
+func TestAttachProc_MultipleSources(t *testing.T) {
+}
+
+func TestAttachProc_ChildKeepsPipeOpen(t *testing.T) {
 }
